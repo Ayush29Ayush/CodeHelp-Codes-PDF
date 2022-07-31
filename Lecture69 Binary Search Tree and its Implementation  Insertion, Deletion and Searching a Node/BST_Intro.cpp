376a6c06162f5db7214a426dea37adfa34d39a2a
Ayush Senapati <ayushsenapati123@gmail.com>
2022-07-31 16:36:38 +0530

    delete a Node

--- Lecture69 Binary Search Tree and its Implementation  Insertion, Deletion and Searching a Node/BST_Intro.cpp
+++ Lecture69 Binary Search Tree and its Implementation  Insertion, Deletion and Searching a Node/BST_Intro.cpp
@@ -70,6 +70,81 @@ Node* insertIntoBST(Node* root, int d)
     return root;
 }
 
+Node* minVal(Node* root)
+{
+    Node* temp = root;
+
+    while(temp->left!=NULL)
+        temp = temp->left;
+
+    return temp;
+}
+
+Node* maxVal(Node* root)
+{
+    Node* temp = root;
+
+    while(temp->right!=NULL)
+        temp = temp->right;
+
+    return temp;
+}
+
+Node* deleteFromBST(Node* root, int val)
+{
+    // base case
+    if(root==NULL)
+        return root;
+
+    if(root->data == val)
+    {
+        //! 0 child
+        if(root->left ==NULL && root->right==NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        //! 1 child
+        //left child
+        if(root->left!=NULL && root->right==NULL)
+        {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        //right child
+        if(root->right!=NULL && root->left==NULL)
+        {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        //! 2 child
+        if(root->left!=NULL && root->right!=NULL)
+        {
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+
+    else if(root->data > val)
+    {
+        // left part mein jao
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+
+    else if(root->data < val)
+    {
+        // right part mein jao
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
 void takeInput(Node* &root)
 {
     int data;
@@ -93,6 +168,19 @@ int main()
 
     cout<<"Printing the BST"<<endl;
     levelOrderTraversal(root);
+
+    cout<< "Min value : "<<minVal(root)->data <<endl;
+    cout<< "Max value : "<<maxVal(root)->data <<endl;
+
+
+    root = deleteFromBST(root, 30);
+    
+
+    cout<<"Printing the BST"<<endl;
+    levelOrderTraversal(root);
+
+    cout<< "Min value : "<<minVal(root)->data <<endl;
+    cout<< "Max value : "<<maxVal(root)->data <<endl;
 
     return 0;
 }
