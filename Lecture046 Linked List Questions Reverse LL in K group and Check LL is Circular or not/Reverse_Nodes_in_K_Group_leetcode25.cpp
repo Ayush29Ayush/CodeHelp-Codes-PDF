fc6cc4ce7e15bbf5023a4c7a59968d2388d88e9e
Ayush Senapati <ayushsenapati123@gmail.com>
2022-08-01 00:06:23 +0530

    updated answer

--- Lecture046 Linked List Questions Reverse LL in K group and Check LL is Circular or not/Reverse_Nodes_in_K_Group_leetcode25.cpp
+++ Lecture046 Linked List Questions Reverse LL in K group and Check LL is Circular or not/Reverse_Nodes_in_K_Group_leetcode25.cpp
@@ -10,6 +10,8 @@
  */
 
 // https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/1914641/C%2B%2B-Easy-Simple-99-faster-recursive-approach-O(1)-space-complexity
+
+// https://youtu.be/fi2vh0nQLi0?t=902
 class Solution
 {
 public:
@@ -19,17 +21,20 @@ public:
             return head;
         if (head == NULL)
             return head;
-        ListNode *current = head, *next = NULL, *prev = NULL, *temp = head;
+        
+        ListNode *current = head, *next = NULL, *prev = NULL; 
+        ListNode *temp = head;
+        
         int size = 0;
         while (temp != NULL)
         {
             size++;
             temp = temp->next;
         }
+        
         if (size < k)
             return head;
-
-            
+        // Step1 : reverse first k nodes
         int count = 0;
         while (count != k and current != NULL)
         {
@@ -39,10 +44,14 @@ public:
             prev = current;
             current = next;
         }
+        
+        // Step2 : recursion dekhlega aage ka
         if (next != NULL)
         {
             head->next = reverseKGroup(next, k);
         }
+        
+        // Step3 : return head of reversed linked list
         return prev;
     }
 };
