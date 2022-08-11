ee4bbe04aa147e237a11f5b5935d6d3816b32f35
Ayush Senapati <ayushsenapati123@gmail.com>
2022-08-11 15:53:53 +0530

    Lec70 BST FAANG QUES PART1

---
+++ Lecture70 Binary SearchTree FAANG Interview Questions  Part-1/Leetcode235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+//! https://youtu.be/pDURIj98e0I?t=1695
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // base case
+        if(root==NULL)
+            return NULL;
+        
+        // if you are in the left part, then find in the right
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        // if you are in the right part, then find in the left
+        else if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else
+            return root;
+    }
+};
