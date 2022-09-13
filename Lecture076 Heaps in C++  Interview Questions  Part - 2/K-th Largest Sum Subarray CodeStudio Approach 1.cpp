4ecf6a7b4b0e01f091a8b8a7f4cc958a14ea424b
Ayush Senapati <ayushsenapati123@gmail.com>
2022-09-13 11:31:12 +0530

    updated

---
+++ Lecture076 Heaps in C++  Interview Questions  Part - 2/K-th Largest Sum Subarray CodeStudio Approach 1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+int getKthLargest(vector<int> &arr, int k)
+{
+	vector<int> sumStore;
+    int n = arr.size();
+    
+    //! Step1
+    // use 2 pointer approach
+    // isse subarray bante jayenge and saath hi saath unke sum leke sumStore pe daaldo
+    for(int i = 0; i<n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j<n; j++)
+        {
+            sum += arr[j];
+            sumStore.push_back(sum);
+        }
+    }
+    //! Step2
+    // sort them to make a ascending order 
+    sort(sumStore.begin(), sumStore.end());
+    
+    //! Step3
+    // return reqd. element
+    return sumStore[sumStore.size() - k];  
+}
